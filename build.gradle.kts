398eeaa30f2323655b1d17a3a1f9bc713e81b859
evecus <jamie55@163.com>
2026-07-24 15:45:26 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
